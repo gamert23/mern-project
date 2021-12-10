a69f58c443d20cbac9766bc8ba949c597575089d
gamert23 <ratchanongame@gmail.com>
2021-12-10 23:23:18 +0700

    Create client side

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Container, Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <h1> Hello World </h1>
+
+      <Button> Bootstrap </Button>
+    </Container>
+  );
+}
+
+export default App;
